485a99cf41b761f67649b157b7ad120ce162e00c
saro928 <sergio_rey992@hotmail.es>
2019-12-18 10:44:18 -0500

    Now using OS Environment Variables for connection with mysqli

--- php/DB.php
+++ php/DB.php
@@ -5,7 +5,14 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$conn = new mysqli("localhost", "root", "", "chat");
+// Get OS Environment Variables for connection
+$host = getenv("DB_HOST");
+$user = getenv("DB_USER");
+$pass = getenv("DB_PASSWORD");
+$db = getenv("DB_DATABASE");
+
+// Create connection
+$conn = new mysqli($host, $user, $pass, $db);
 // Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
